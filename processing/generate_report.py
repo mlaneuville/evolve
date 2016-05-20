c1ecb03b67743464917cf0eaa004bbd6cc11e48d
Matthieu Laneuville <mlaneuville@gmail.com>
2016-05-20 16:29:56 +0900

    pylint compliant

--- processing/generate_report.py
+++ processing/generate_report.py
@@ -2,23 +2,23 @@ import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
 
-parser = argparse.ArgumentParser()
-parser.add_argument("datafile")
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument("datafile")
 
-args = parser.parse_args()
+ARGS = PARSER.parse_args()
 
-print(args.datafile)
+print(ARGS.datafile)
 
-df = pd.read_csv(args.datafile)
-print(df.head())
+DF = pd.read_csv(ARGS.datafile)
+print(DF.head())
 
-for feature in df:
+for feature in DF:
     if feature == "iter" or feature == "time":
         continue
-    if df[feature].mean() > 0.:
-        plt.plot(df['time'], df[feature], lw=2, label=feature)
+    if DF[feature].mean() > 0.:
+        plt.plot(DF['time'], DF[feature], lw=2, label=feature)
 
 plt.legend(loc='best')
 plt.yscale('log')
 plt.grid()
-plt.savefig("report.png", format="png", bbox_inches="tight")
+plt.savefig("report.pdf", format="pdf", bbox_inches="tight")
