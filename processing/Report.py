c457bbd31a06df3d2b301a05fd7fcb330e5e58e1
Matthieu Laneuville <mlaneuville@gmail.com>
2016-05-23 10:56:24 +0900

    now outputs in single pdf

--- processing/Report.py
+++ processing/Report.py
@@ -4,22 +4,49 @@
 """
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 
 class Report:
     def __init__(self, name, sim_dir="../output"):
         self.fname = sim_dir + "/out_" + name + ".txt"
         self.data = self.load_data(self.fname)
+        self.output_file = "report_"+name+".pdf"
+        self.timeseries = {}
         print(self.data.info())
+
 
     def load_data(self, fname):
         return pd.read_csv(fname)
 
-    def plot_timeseries(self, name, ylabel="", isLog=False):
-        plt.plot(self.data['time'][1:], self.data[name][1:], lw=2)
-        plt.ylabel(ylabel)
-        plt.xlabel("Time since formation [Ma]")
-        plt.grid()
-        if isLog:
-            plt.yscale('log')
-        plt.savefig("report_"+name+".pdf")
-        plt.close()
+
+    def add_timeseries(self, name, colnames=[], ylabel="", isLog=False):
+        fig, ax = plt.subplots(1, 1)
+        if len(colnames) == 0:
+            colnames = [name]
+        self.timeseries[name] = {'fig':fig, 'ax':ax, 'ylabel':ylabel,
+                                 'isLog':isLog, 'colnames':colnames} 
+
+
+    def process(self):
+
+        fig, ax = plt.subplots(1, 1)
+
+
+        for k, v in self.timeseries.items():
+            print("Plotting "+k)
+            for column in v['colnames']:
+                v['ax'].plot(self.data['time'][1:], self.data[column][1:], lw=2)
+            v['ax'].set_ylabel(v['ylabel'])
+            v['ax'].set_xlabel("Time since formation [Ma]")
+            v['ax'].set_title(k)
+            v['ax'].grid()
+
+            if v['isLog']:
+                v['ax'].set_yscale('log')
+
+
+    def to_file(self):
+        with PdfPages(self.output_file) as pdf:
+            for k, v in self.timeseries.items():
+                pdf.savefig(v['fig'])
+                plt.close()
--- processing/generate_report.py
+++ processing/generate_report.py
@@ -2,5 +2,11 @@ from Report import *
 
 r = Report("")
 
-r.plot_timeseries("Impacts", ylabel="Production flux [kg N/yr]", isLog=True)
-r.plot_timeseries("AbioticFixation", ylabel="Production flux [kg N/yr]", isLog=True)
+r.add_timeseries("Impacts", ylabel="Production flux [kg N/yr]", isLog=True)
+r.add_timeseries("AbioticFixation",  ylabel="Production flux [kg N/yr]", isLog=True)
+
+r.add_timeseries("Test", ["Impacts", "AbioticFixation"],  
+                 ylabel="Production flux [kg N/yr]", isLog=True)
+
+r.process()
+r.to_file()
