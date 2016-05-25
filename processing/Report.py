dc3e8500ce028df7b38154eea71c4ca691dac381
Matthieu Laneuville <mlaneuville@gmail.com>
2016-05-25 11:51:37 +0900

    major report update

--- processing/Report.py
+++ processing/Report.py
@@ -4,41 +4,55 @@
 """
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 from matplotlib.backends.backend_pdf import PdfPages
 from collections import OrderedDict
+from PyPDF2 import PdfFileReader, PdfFileMerger
+import graphviz
+import os
 
 class Report:
     def __init__(self, name, sim_dir="../output"):
+        self.graph_fname = sim_dir + "/out_" + name + ".dot"
         self.fname = sim_dir + "/out_" + name + ".txt"
         self.data = self.load_data(self.fname)
         self.output_file = "report_"+name+".pdf"
         self.timeseries = OrderedDict()
-        print(self.data.info())
+        print(self.data.head())
 
 
     def load_data(self, fname):
         return pd.read_csv(fname)
 
 
+    def get_label(self, name):
+        species = {"0": "N2", "1": "NOx", "2": "NH4"}
+        label = name
+        if name[-1].isdigit():
+            label = name[:-1] + " -- " + species[name[-1]]
+        return label
+
+
     def add_timeseries(self, name, colnames=[], ylabel="", isLog=False, norm=1):
-        fig, ax = plt.subplots(1, 1)
         if len(colnames) == 0:
             colnames = [name]
+
+        fig, ax = plt.subplots(1, 1)
         self.timeseries[name] = {'fig':fig, 'ax':ax, 'ylabel':ylabel,
                                  'isLog':isLog, 'colnames':colnames, 'norm':norm} 
 
 
     def process(self):
 
-        fig, ax = plt.subplots(1, 1)
-
-
         for k, v in self.timeseries.items():
             print("Plotting "+k)
             for column in sorted(v['colnames']):
-                v['ax'].plot(self.data['time'][1:], 
-                             self.data[column][1:]/v['norm'], 
-                             lw=2, label=column)
+                label = self.get_label(column)
+                data = self.data[column][1:]/v['norm']
+                if v['isLog']:
+                    data = abs(data)
+                v['ax'].plot(self.data['time'][1:], data, 
+                             lw=2, label=label)
 
             v['ax'].legend(loc='best')
             v['ax'].set_ylabel(v['ylabel'])
@@ -51,7 +65,21 @@ class Report:
 
 
     def to_file(self):
+
+        graph_src = open(self.graph_fname, "r")
+        dot = graphviz.Source(graph_src.read(), engine="dot", format="pdf")
+        dot.render()
+
         with PdfPages(self.output_file) as pdf:
             for k, v in self.timeseries.items():
                 pdf.savefig(v['fig'])
                 plt.close()
+
+        graph = PdfFileReader("Source.gv.pdf")
+        report = PdfFileReader(self.output_file)
+        merger = PdfFileMerger()
+        merger.append(graph)
+        merger.append(report)
+        merger.write(self.output_file)
+        os.remove("Source.gv.pdf")
+        os.remove("Source.gv")
--- processing/generate_report.py
+++ processing/generate_report.py
@@ -1,13 +1,35 @@
+import argparse
 from Report import *
 
-r = Report("")
+parser = argparse.ArgumentParser()
+parser.add_argument('id', type=str, help='Case ID, useful if there is more than one in folder')
+
+args = parser.parse_args()
+
+r = Report(args.id)
 
 r.add_timeseries("Impacts", ylabel="Production flux [kg N/yr]", isLog=True)
-r.add_timeseries("AbioticFixation",  ylabel="Production flux [kg N/yr]", isLog=True)
+r.add_timeseries("CometDelivery", ylabel="Production flux [kg N/yr]", isLog=True)
+r.add_timeseries("AbioticFixation", ylabel="Production flux [kg N/yr]", isLog=True)
+r.add_timeseries("Henry", ylabel="Production flux [kg N/yr]", isLog=True)
+r.add_timeseries("FreundlichAdsorption", ylabel="Production flux [kg N/yr]", isLog=True)
+r.add_timeseries("HydrothermalCirculation", ylabel="Production flux [kg N/yr]", isLog=True)
+r.add_timeseries("Subduction", ylabel="Production flux [kg N/yr]", isLog=True)
+r.add_timeseries("Convection", ylabel="Production flux [kg N/yr]", isLog=True)
+r.add_timeseries("Volcanism", ylabel="Production flux [kg N/yr]", isLog=True)
 
-r.add_timeseries("Global evolution", 
-                 ["Oceans1", "Oceans2", "LMantle2", "UMantle2", "OCrust2", "Atmosphere0"],  
-                 ylabel="Nitrogen content [kg N]", isLog=True, norm=1e21)
+
+r.add_timeseries("Atmosphere evolution", ["Atmosphere0", "Atmosphere1"],  
+                 ylabel="Nitrogen content [PAL]", norm=2e19)
+
+r.add_timeseries("Ocean evolution", ["Oceans0", "Oceans1", "Oceans2"],  
+                 ylabel="Nitrogen content [umol/L]", isLog=True, norm=1.8e13)
+
+r.add_timeseries("Crust evolution", ["OCrust2"],  
+                 ylabel="Nitrogen content [kg N]", isLog=True)
+
+r.add_timeseries("Mantle evolution", ["UMantle2", "LMantle2"],  
+                  ylabel="Nitrogen content [kg N]", isLog=True)
 
 r.process()
 r.to_file()
