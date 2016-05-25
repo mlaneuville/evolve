c89dcdd32434ae2eb7eaae6990958f41d197ae8f
Matthieu Laneuville <mlaneuville@gmail.com>
2016-05-25 16:12:10 +0900

    added possibility to have subplots generated directly

--- processing/Report.py
+++ processing/Report.py
@@ -3,6 +3,7 @@
 @author: mlaneuville
 """
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from matplotlib.backends.backend_pdf import PdfPages
@@ -10,6 +11,7 @@ from collections import OrderedDict
 from PyPDF2 import PdfFileReader, PdfFileMerger
 import graphviz
 import os
+import sys
 
 class Report:
     def __init__(self, name, sim_dir="../output"):
@@ -18,6 +20,7 @@ class Report:
         self.data = self.load_data(self.fname)
         self.output_file = "report_"+name+".pdf"
         self.timeseries = OrderedDict()
+        self.subplots = []
         print(self.data.head())
 
 
@@ -28,8 +31,13 @@ class Report:
     def get_label(self, name):
         species = {"0": "N2", "1": "NOx", "2": "NH4"}
         label = name
-        if name[-1].isdigit():
-            label = name[:-1] + " -- " + species[name[-1]]
+        if isinstance(name, list):
+            label = []
+            for value in name:
+                if value[-1].isdigit():
+                    label.append(value[:-1] + " -- " + species[value[-1]])
+                else:
+                    label.append(value)
         return label
 
 
@@ -44,6 +52,29 @@ class Report:
         self.timeseries[name] = {'fig':fig, 'ax':ax, 'ylabel':ylabel,
                                  'isLog':isLog, 'colnames':colnames, 'norm':norm} 
 
+
+    def add_subplot(self, plots):
+        if len(plots) == 0 or len(plots) > 4:
+            print("Wrong number of plots: "+str(len(plots)))
+            sys.exit()
+
+        fig, axarr = plt.subplots(2, 2, sharex=True)
+
+        cols = []
+        ylabels = []
+        norms = []
+        islog = []
+        for k, plot in plots.items():
+            cols.append(plot['cols'])
+            ylabels.append(plot['ylabel'])
+
+            norm = [1] if 'norm' not in plot.keys() else plot['norm']
+            norms.append(norm)
+
+            log = True if 'isLog' in plot.keys() else False
+            islog.append(log)
+
+        self.subplots.append({'fig':fig, 'ax':axarr, 'cols':cols, 'ylabels':ylabels, 'norms':norms, 'isLog':islog}) 
 
     def process(self):
 
@@ -66,6 +97,23 @@ class Report:
             if v['isLog']:
                 v['ax'].set_yscale('log')
 
+        for sub in self.subplots:
+            for i, (col, norm, log) in enumerate(zip(sub['cols'], sub['norms'], sub['isLog'])):
+                idx = (i/2, i%2)
+                data = self.data[col][1:]/norm
+                label = self.get_label(col)
+                sub['ax'][idx].plot(self.data['time'][1:], data, lw=2)
+                sub['ax'][idx].set_xticks(np.arange(0, 4500, 1000))
+                sub['ax'][idx].set_ylabel(sub['ylabels'][i])
+                leg = sub['ax'][idx].legend(labels=label, loc='best')
+                leg.draw_frame(False)
+                if log:
+                    sub['ax'][idx].set_yscale('log')
+                sub['ax'][idx].grid()
+                sub['fig'].tight_layout()
+                if i >= 2:
+                    sub['ax'][idx].set_xlabel("Time since formation [Ma]")
+
 
     def to_file(self):
 
@@ -77,6 +125,11 @@ class Report:
             for k, v in self.timeseries.items():
                 pdf.savefig(v['fig'])
                 plt.close()
+
+            for v in self.subplots:
+                pdf.savefig(v['fig'])
+                plt.close()
+
 
         graph = PdfFileReader("Source.gv.pdf")
         report = PdfFileReader(self.output_file)
--- processing/generate_report.py
+++ processing/generate_report.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 import argparse
 from Report import *
 
@@ -8,28 +9,63 @@ args = parser.parse_args()
 
 r = Report(args.id)
 
-r.add_timeseries("Impacts", ylabel="Production flux [kg N/yr]", isLog=True)
-r.add_timeseries("CometDelivery", ylabel="Production flux [kg N/yr]", isLog=True)
-r.add_timeseries("AbioticFixation", ylabel="Production flux [kg N/yr]")
-r.add_timeseries("Henry", ylabel="Production flux [kg N/yr]")
-r.add_timeseries("FreundlichAdsorption", ylabel="Production flux [kg N/yr]")
-r.add_timeseries("HydrothermalCirculation", ylabel="Production flux [kg N/yr]")
-r.add_timeseries("Subduction", ylabel="Production flux [kg N/yr]")
-r.add_timeseries("Convection", ylabel="Production flux [kg N/yr]")
-r.add_timeseries("Volcanism", ylabel="Production flux [kg N/yr]")
+#r.add_timeseries("Impacts", ylabel="Production flux [kg N/yr]", isLog=True)
+#r.add_timeseries("CometDelivery", ylabel="Production flux [kg N/yr]", isLog=True)
+#r.add_timeseries("AbioticFixation", ylabel="Production flux [kg N/yr]")
+#r.add_timeseries("Henry", ylabel="Production flux [kg N/yr]")
+#r.add_timeseries("FreundlichAdsorption", ylabel="Production flux [kg N/yr]")
+#r.add_timeseries("HydrothermalCirculation", ylabel="Production flux [kg N/yr]")
+#r.add_timeseries("Subduction", ylabel="Production flux [kg N/yr]")
+#r.add_timeseries("Convection", ylabel="Production flux [kg N/yr]")
+#r.add_timeseries("Volcanism", ylabel="Production flux [kg N/yr]")
 
+plots_fluxes = OrderedDict()
 
-r.add_timeseries("Atmosphere evolution", ["Atmosphere0"],  
-                 ylabel="Nitrogen content [PAL]", norm=[2e19])
+plots_fluxes["Atmosphere"] = {'cols':["Impacts", "CometDelivery"],
+                             'ylabel':"Nitrogen flux [kg N/yr]", 'isLog':True} 
 
-r.add_timeseries("Ocean evolution", ["Oceans0", "Oceans1", "Oceans2"],  
-                 ylabel="Nitrogen content [umol/L]", isLog=True, norm=[1.8e13])
+plots_fluxes["Oceans1"] = {'cols':["FreundlichAdsorption", "HydrothermalCirculation"],
+                             'ylabel':"Nitrogen flux [kg N/yr]"} 
 
-r.add_timeseries("Crust evolution", ["OCrust2"],  
-                 ylabel="Nitrogen content [ppm]", norm=[1.0e15])
+plots_fluxes["Oceans2"] = {'cols':["AbioticFixation", "Henry"],
+                             'ylabel':"Nitrogen flux [kg N/yr]"} 
 
-r.add_timeseries("Mantle evolution", ["UMantle2", "LMantle2"],  
-                  ylabel="Nitrogen content [ppm]", norm=[8.2e17, 2.2e18])
+plots_fluxes["Interior"] = {'cols':["Subduction", "Convection", "Volcanism"],
+                             'ylabel':"Nitrogen flux [kg N/yr]"} 
+
+r.add_subplot(plots_fluxes)
+
+#r.add_timeseries("Atmosphere evolution", ["Atmosphere0"],  
+#                 ylabel="Nitrogen content [PAL]", norm=[2e19])
+#
+#r.add_timeseries("Ocean evolution", ["Oceans0", "Oceans1", "Oceans2"],  
+#                 ylabel="Nitrogen content [umol/L]", isLog=True, norm=[1.8e13])
+#
+#r.add_timeseries("Crust evolution", ["OCrust2"],  
+#                 ylabel="Nitrogen content [ppm]", norm=[1.0e15])
+#
+#r.add_timeseries("Mantle evolution", ["UMantle2", "LMantle2"],  
+#                  ylabel="Nitrogen content [ppm]", norm=[8.2e17, 2.2e18])
+
+plots_evo = OrderedDict()
+
+plots_evo["Atmosphere"] = {'cols':["Atmosphere0"], 
+                       'ylabel':"Nitrogen content [PAL]", 
+                       'norm':[2e19]}
+
+plots_evo["Oceans"] = {'cols':["Oceans0", "Oceans1", "Oceans2"], 
+                   'ylabel':"Nitrogen content [umol/L]", 
+                   'norm':[1.8e13], 'isLog':True}
+
+plots_evo["Crust"] = {'cols':["OCrust2"], 
+                  'ylabel':"Nitrogen content [ppm]", 
+                  'norm':[1.0e15]}
+
+plots_evo["Mantle"] = {'cols':["UMantle2", "LMantle2"], 
+                   'ylabel':"Nitrogen content [ppm]", 
+                   'norm':[8.2e17, 2.2e18]}
+
+r.add_subplot(plots_evo)
 
 r.process()
 r.to_file()
