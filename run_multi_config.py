4c8dea7ae83199be20fbf17594b9d1ca34d1384e
Matthieu Laneuville <mlaneuville@gmail.com>
2016-10-11 11:32:53 +0900

    added scripts to run many simulations at once

--- run_multi_config.py
+++ run_multi_config.py
@@ -1,3 +1,8 @@
+'''
+Run all yaml files from a given folder if the corresponding out file doesn't
+already exist. Four parallel threads are used by default.
+'''
+
 from glob import glob
 import argparse
 import logging
@@ -6,27 +11,36 @@ import os
 import time
 import subprocess
 
-logging.basicConfig(level=logging.DEBUG, 
+logging.basicConfig(level=logging.DEBUG,
                     format="%(asctime)s (%(threadName)-2s) %(message)s")
 
 FNULL = open(os.devnull, "w")
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--folder', default='output') 
-args = parser.parse_args()
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument('-f', '--folder', default='output')
+ARGS = PARSER.parse_args()
 
-config = glob(args.folder+"/*.yaml")
-print(config)
+CONFIG = glob(ARGS.folder+"/*.yaml")
+for i, yaml in enumerate(CONFIG):
+    idx = yaml.split("config_")
+    fname = "out_%s.txt" % (idx[1][:-5])
+    fullname = os.getcwd()+"/"+ARGS.folder+"/"+fname
+    if os.path.isfile(fullname):
+        CONFIG.pop(i)
 
-def worker(s, conf):
+print(CONFIG)
+
+def worker(sema_, conf_):
+    '''Define worker's work.'''
     name = threading.currentThread().getName()
-    logging.debug("Worker %02d waiting to join the pool" % int(name))
-    with s:
-        subprocess.call(["./evolve", conf], stdout=FNULL)
+    logging.debug("Worker %02d waiting to join the pool", int(name))
+    with sema_:
+        logging.debug("%s starting", conf_)
+        subprocess.call(["./evolve", conf_], stdout=FNULL)
+        logging.debug("%s is done!", conf_)
 
-s = threading.Semaphore(4)
-for i, conf in enumerate(config):
-    print(conf)
-    t = threading.Thread(target=worker, name=str(i), args=(s, conf,))
+SEMA = threading.Semaphore(4)
+for i, conf in enumerate(CONFIG):
+    t = threading.Thread(target=worker, name=str(i), args=(SEMA, conf,))
     time.sleep(0.2)
     t.start()
