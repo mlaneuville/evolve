2c92b5164702f5d6885ce65c7f65f3f8c1907ed2
Matthieu Laneuville <mlaneuville@gmail.com>
2016-10-12 11:16:54 +0900

    minor cleanup and pylint consistent

--- processing/generate_heatmap.py
+++ processing/generate_heatmap.py
@@ -1,50 +1,67 @@
+'''
+Generates a series of heatmaps to show the distribution of nitrogen content in a
+given reservoir (x-axis) as a function of redox parameter (y-axis). The color
+scale is a histogram of the distribution of random runs for a given redox value.
+'''
+
 from glob import glob
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
-import argparse
 import numpy as np
-import os
 
-columns = ["Atmosphere0", "Oceans0", "Oceans1", "Oceans2", "LMantle2", "UMantle2", "OCrust2"]
+COLUMNS = ["Atmosphere0", "Oceans0", "Oceans1", "Oceans2", "LMantle2", "UMantle2", "OCrust2"]
 
-units = {"Atmosphere0": "PAL",
-         "Oceans0": "mM", 
-         "Oceans1": "mM", 
-         "Oceans2": "mM", 
-         "LMantle2": "ppm", 
-         "UMantle2": "ppm", 
+UNITS = {"Atmosphere0": "PAL",
+         "Oceans0": "mM",
+         "Oceans1": "mM",
+         "Oceans2": "mM",
+         "LMantle2": "ppm",
+         "UMantle2": "ppm",
          "OCrust2": "ppm"}
 
-norms = {"Atmosphere0": 4e18,  # PAL
+NORMS = {"Atmosphere0": 4e18,  # PAL
          "Oceans0": 3.8e16,    # mM ; molar = m*1e3 g/(28g/mol)/1.35e21 L
          "Oceans1": 5.1e16,    # mM ; average of 30 and 46 g/mol
          "Oceans2": 2.4e16,    # mM ; average of 17 and 18 g/mol
          "LMantle2": 2.5e18,   # ppm ; v(lmantle) = 6.3e20 m3, rho = 4000 kg/m3
-         "UMantle2": 9.6e17,   # ppm ; v(umantle) = 2.4e20 m3, rho = 4000 kg/m3 
+         "UMantle2": 9.6e17,   # ppm ; v(umantle) = 2.4e20 m3, rho = 4000 kg/m3
          "OCrust2": 1e15}      # ppm ; msed = 1.04e21 kg
 
 DATA_DIRECTORY = "../output/"
 
-def make_time_plot(field, norm, time):
+def get_full_data(field_, norm, time):
+    '''Get data (time and field vectors) for a given time. Fetches output from
+       all folders in DATA_DIRECTORY.'''
+
     xval = []
     yval = []
 
     for folder in sorted(os.listdir(DATA_DIRECTORY)):
         if not os.path.isdir(DATA_DIRECTORY+folder):
             continue
-    
+
         folder = DATA_DIRECTORY + folder
         ndata = len(glob(folder+'/*.txt'))
-    
+
         for data_file in glob(folder+'/*.txt'):
-            df = pd.read_csv(data_file)
+            dataframe = pd.read_csv(data_file)
             oxi = int(data_file.split("/")[2][4:7])
 
-            reservoir = df[field].values[time]
-            simtime = df["time"].values[time]
+            reservoir = dataframe[field_].values[time]
+            simtime = dataframe["time"].values[time]
 
             xval.append(reservoir/norm)
             yval.append(oxi)
+
+    return xval, yval, simtime, ndata
+
+
+
+def make_time_plot(field_, norm, time):
+    '''Makes the heatmap for a given time and field.'''
+
+    xval, yval, simtime, ndata = get_full_data(field_, norm, time)
 
     print(np.min(xval))
     print(np.max(xval))
@@ -52,39 +69,37 @@ def make_time_plot(field, norm, time):
     print(np.min(yval))
     print(np.max(yval))
 
-    
     plt.figure()
 
-    xr = np.logspace(-2, 2, 26)
-    yr = np.linspace(75, 100, 26)
-    counts, _, _ = np.histogram2d(xval, yval, bins=(xr, yr))
+    xbins = np.logspace(-2, 2, 26)
+    ybins = np.linspace(75, 100, 26)
+    counts, _, _ = np.histogram2d(xval, yval, bins=(xbins, ybins))
     print(counts.shape)
 
-#    xr = np.linspace(0, 5, len(xr)-1)
-#    yr = np.linspace(75, 100, len(yr)-1)
-#    X, Y = np.meshgrid(xr, yr)
-
-    levels = np.linspace(0, 0.5, 101)
-#    plt.contourf(counts, levels)
-    plt.pcolormesh(xr, yr, counts.T)
+    plt.pcolormesh(xbins, ybins, counts.T)
     plt.colorbar()
     plt.clim(0, ndata)
-    
+
     plt.xscale('log')
 
-    plt.title("%s - %04d Ma" % (field, simtime))
-    plt.xlabel("N-content [%s]" % units[field])
+    plt.title("%s - %04d Ma" % (field_, simtime))
+    plt.xlabel("N-content [%s]" % UNITS[field_])
     plt.ylabel("Redox state [-]")
-    plt.savefig("%s-%03d.png" % (field, time), format="png")
+    plt.savefig("%s-%03d.png" % (field_, time), format="png")
     plt.close()
 
-def run_timeloop(field):
-    print(field)
-    norm = norms[field]
+
+
+def run_timeloop(field_):
+    '''Makes all heatmaps for a given field'''
+    print(field_)
+    norm = NORMS[field_]
     for i in range(180):
         print(i)
-        make_time_plot(field, norm, i)
+        make_time_plot(field_, norm, i)
+
+
 
 #run_timeloop("Oceans1")
-for field in columns:
+for field in COLUMNS:
     run_timeloop(field)
--- processing/generate_report.py
+++ processing/generate_report.py
@@ -1,88 +1,71 @@
+'''
+Generate fluxes and evolution plots for a given run. If with_graph is set, will
+also include the network graph in the output pdf.
+'''
+
 from collections import OrderedDict
-import argparse
-from Report import *
 from glob import glob
+import argparse
+from Report import Report
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--id', default=False, type=str, help='Case ID, useful if there is more than one in folder')
-parser.add_argument('-f', '--folder', default='output/', type=str, help='folder where out_* are stored')
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument('-i', '--id', default=False, type=str,
+                    help='Case ID, useful if there is more than one in folder')
+PARSER.add_argument('-f', '--folder', default='output/', type=str,
+                    help='folder where out_* are stored')
 
-args = parser.parse_args()
+ARGS = PARSER.parse_args()
 
+PLOTS_FLUXES = OrderedDict()
+PLOTS_FLUXES["Atmosphere"] = {'cols':["Impacts", "CometDelivery"],
+                              'ylabel':"Nitrogen flux [kg N/yr]", 'isLog':True}
 
-#r.add_timeseries("Impacts", ylabel="Production flux [kg N/yr]", isLog=True)
-#r.add_timeseries("CometDelivery", ylabel="Production flux [kg N/yr]", isLog=True)
-#r.add_timeseries("AbioticFixation", ylabel="Production flux [kg N/yr]")
-#r.add_timeseries("Henry", ylabel="Production flux [kg N/yr]")
-#r.add_timeseries("FreundlichAdsorption", ylabel="Production flux [kg N/yr]")
-#r.add_timeseries("HydrothermalCirculation", ylabel="Production flux [kg N/yr]")
-#r.add_timeseries("Subduction", ylabel="Production flux [kg N/yr]")
-#r.add_timeseries("Convection", ylabel="Production flux [kg N/yr]")
-#r.add_timeseries("Volcanism", ylabel="Production flux [kg N/yr]")
+PLOTS_FLUXES["Oceans1"] = {'cols':["FreundlichAdsorption", "HydrothermalCirculation"],
+                           'ylabel':"Nitrogen flux [kg N/yr]", 'isLog':True}
 
-plots_fluxes = OrderedDict()
+PLOTS_FLUXES["Oceans2"] = {'cols':["AbioticFixation", "Henry"],
+                           'ylabel':"Nitrogen flux [kg N/yr]"}
 
-plots_fluxes["Atmosphere"] = {'cols':["Impacts", "CometDelivery"],
-                             'ylabel':"Nitrogen flux [kg N/yr]", 'isLog':True} 
+PLOTS_FLUXES["Interior"] = {'cols':["Subduction", "Convection", "Volcanism"],
+                            'ylabel':"Nitrogen flux [kg N/yr]", 'isLog':True}
 
-plots_fluxes["Oceans1"] = {'cols':["FreundlichAdsorption", "HydrothermalCirculation"],
-                             'ylabel':"Nitrogen flux [kg N/yr]", 'isLog':True} 
+# TODO: norms should match that in generate_heatmap
 
-plots_fluxes["Oceans2"] = {'cols':["AbioticFixation", "Henry"],
-                             'ylabel':"Nitrogen flux [kg N/yr]"} 
+PLOTS_EVO = OrderedDict()
+PLOTS_EVO["Atmosphere"] = {'cols':["Atmosphere0"],
+                           'ylabel':"Nitrogen content [PAL]",
+                           'norm':[4e18]}
 
-plots_fluxes["Interior"] = {'cols':["Subduction", "Convection", "Volcanism"],
-                             'ylabel':"Nitrogen flux [kg N/yr]", 'isLog':True} 
+PLOTS_EVO["Oceans"] = {'cols':["Oceans0", "Oceans1", "Oceans2"],
+                       'ylabel':"Nitrogen content [mmol/L]",
+                       'norm':[1.8e16], 'isLog':True}
 
+PLOTS_EVO["Crust"] = {'cols':["OCrust2"],
+                      'ylabel':"Nitrogen content [ppm]",
+                      'norm':[1.0e15], 'isLog':True}
 
-#r.add_timeseries("Atmosphere evolution", ["Atmosphere0"],  
-#                 ylabel="Nitrogen content [PAL]", norm=[2e19])
-#
-#r.add_timeseries("Ocean evolution", ["Oceans0", "Oceans1", "Oceans2"],  
-#                 ylabel="Nitrogen content [umol/L]", isLog=True, norm=[1.8e13])
-#
-#r.add_timeseries("Crust evolution", ["OCrust2"],  
-#                 ylabel="Nitrogen content [ppm]", norm=[1.0e15])
-#
-#r.add_timeseries("Mantle evolution", ["UMantle2", "LMantle2"],  
-#                  ylabel="Nitrogen content [ppm]", norm=[8.2e17, 2.2e18])
+PLOTS_EVO["Mantle"] = {'cols':["UMantle2", "LMantle2"],
+                       'ylabel':"Nitrogen content [ppm]",
+                       'norm':[8.2e17, 2.2e18], 'isLog':True}
 
-plots_evo = OrderedDict()
+if ARGS.id:
+    r = Report(ARGS.id, sim_dir=ARGS.folder)
 
-plots_evo["Atmosphere"] = {'cols':["Atmosphere0"], 
-                       'ylabel':"Nitrogen content [PAL]", 
-                       'norm':[4e18]}
-
-plots_evo["Oceans"] = {'cols':["Oceans0", "Oceans1", "Oceans2"], 
-                   'ylabel':"Nitrogen content [mmol/L]", 
-                   'norm':[1.8e16], 'isLog':True}
-
-plots_evo["Crust"] = {'cols':["OCrust2"], 
-                  'ylabel':"Nitrogen content [ppm]", 
-                  'norm':[1.0e15], 'isLog':True}
-
-plots_evo["Mantle"] = {'cols':["UMantle2", "LMantle2"], 
-                   'ylabel':"Nitrogen content [ppm]", 
-                   'norm':[8.2e17, 2.2e18], 'isLog':True}
-
-if args.id:
-    r = Report(args.id, sim_dir=args.folder)
-
-    r.add_subplot(plots_fluxes)
-    r.add_subplot(plots_evo)
+    r.add_subplot(PLOTS_FLUXES)
+    r.add_subplot(PLOTS_EVO)
 
     r.process()
     r.to_file()
 else:
-    output = glob("../"+args.folder+"/*.txt")
-    for out in output:
+    OUTPUT = glob(ARGS.folder+"/*.txt")
+    for out in OUTPUT:
         file_id = out.split("out_")[-1]
         file_id = file_id.split(".txt")[0]
 
-        r = Report(file_id, sim_dir='../'+args.folder)
-    
-        r.add_subplot(plots_fluxes)
-        r.add_subplot(plots_evo)
-    
+        r = Report(file_id, sim_dir=ARGS.folder)
+
+        r.add_subplot(PLOTS_FLUXES)
+        r.add_subplot(PLOTS_EVO)
+
         r.process()
-        r.to_file()
+        r.to_file(with_graph=False)
