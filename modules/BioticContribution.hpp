ecfe698b718c3076d3563611140d20970ecb688c
Matthieu Laneuville <mlaneuville@gmail.com>
2017-04-21 11:40:59 +0900

    new biotic contribution implementation

--- modules/BioticContribution.hpp
+++ modules/BioticContribution.hpp
@@ -2,33 +2,40 @@ class BioticContribution: public Module {
 public:
     BioticContribution(string name): Module(name) { init(); }
 
-    double E6i, E6f;
+    bool const_E6;
+    double E6i, MMAX;
+
+    double f3, f4, f5, f6;
     double dbio, onset, E3, E4, E5, E6;
     double EA, ED;
-    bool efficiency;
 
     void init(void) {
         this->links.push_back("Oceans1 -> Atmosphere0");
         this->links.push_back("Oceans2 -> Atmpsphere0");
         this->links.push_back("Oceans1 -> OCrust2");
         this->links.push_back("Oceans2 -> OCrust2");
-        this->numOutputs = 3;
-        this->init_fluxes(3);
+        this->numOutputs = 4;
+        this->init_fluxes(4);
 
 
         // dbio is in kg/year
-        efficiency = config->data["BioticContribution"]["efficiency"].as<bool>();
+        const_E6 = config->data["BioticContribution"]["const_E6"].as<bool>();
         dbio = config->data["BioticContribution"]["dbio"].as<double>();
         onset = config->data["BioticContribution"]["onset"].as<double>();
         ED = config->data["BioticContribution"]["ED"].as<double>();
         EA = config->data["BioticContribution"]["EA"].as<double>();
         E6i = config->data["BioticContribution"]["E6i"].as<double>();
-        E6f = config->data["BioticContribution"]["E6f"].as<double>();
+        MMAX = config->data["BioticContribution"]["MMAX"].as<double>();
 
         E6 = E6i;
         E5 = E6;
         E4 = E6*6;
         E3 = E6*2;
+
+        f3 = 1.0;
+        f4 = 1.0;
+        f5 = 1.0;
+        f6 = 1.0;
     }
 
     void evolve(void) {
@@ -43,8 +50,9 @@ public:
             return;
         }
 
-        if (efficiency) {
-            E6 = E6i + (E6f - E6i)*(s->time - onset)/(4.5e9 - onset);
+        if (!const_E6) {
+            //E6 = E6i * exp(-s->masses[atm+2]/MMAX);
+            E6 = E6i * (MMAX-s->masses[atm+2])/MMAX;
             E5 = E6;
             E4 = E6*6;
             E3 = E6*2;
@@ -80,13 +88,13 @@ public:
         double spathway = -1;
         double scale = 1; //s->masses[atm+2]/1e16;
         if (not synthesis and ED > 0 and EA > 0) { // F6
-            ED -= bio_increase/E6; 
-            EA -= bio_increase/E6;
+            ED -= f6*bio_increase/E6; 
+            EA -= f6*bio_increase/E6;
             synthesis = true;
             spathway = 6;
         }
         if (not synthesis and ED <= 0 and EA > 0) { // F3
-            flux = bio_increase/E3;
+            flux = f3*bio_increase/E3;
             EA -= flux; 
             if (s->masses[oc+2] < 2*flux) {
                 s->fluxes[atm+2] -= s->masses[atm+2]*0.01/s->timestep;
@@ -99,7 +107,7 @@ public:
             spathway = 3;
         }
         if (not synthesis and ED > 0 and EA <= 0) { // F4
-            flux = bio_increase/E4;
+            flux = f4*bio_increase/E4;
             ED -= flux; 
             if (s->masses[oc+1] < 2*flux) {
                 s->fluxes[atm+2] -= s->masses[atm+2]*0.01/s->timestep;
@@ -112,10 +120,10 @@ public:
             spathway = 4;
         }
         if (not synthesis and ED <= 0 and EA <= 0) { // F5
-            flux = bio_increase/E5;
+            flux = f5*bio_increase/E5;
             if (s->masses[oc+2] < 2*flux or s->masses[oc+1] < 2*flux) {
-                s->fluxes[atm+2] -= s->masses[atm+2]*0.1/s->timestep;
-                s->fluxes[oc+2] += s->masses[atm+2]*0.1/s->timestep;
+                s->fluxes[atm+2] -= s->masses[atm+2]*0.01/s->timestep;
+                s->fluxes[oc+2] += s->masses[atm+2]*0.01/s->timestep;
             } else {
                 s->fluxes[oc+1] -= 0.5*scale*flux/s->timestep; 
                 s->fluxes[oc+2] -= 0.5*scale*flux/s->timestep;
@@ -129,7 +137,7 @@ public:
             cout << "BioticContribution: BS=" << s->masses[atm+2] << endl;
         }
 
-        vector<double> output = {s->masses[atm+2], spathway, apathway};
+        vector<double> output = {s->masses[atm+2], spathway, apathway, flux/s->timestep};
         this->fluxes.push_back(output);
     }
 
