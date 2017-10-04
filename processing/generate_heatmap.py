e93d1fe85967a42fdfb6f49b329e07a1359a618c
Matthieu Laneuville <mlaneuville@gmail.com>
2017-10-04 15:30:25 +0900

    processing: update heatmap script with latest version of the code

--- processing/generate_heatmap.py
+++ processing/generate_heatmap.py
@@ -11,9 +11,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-COLUMNS = ["Atmosphere0", "Oceans0", "Oceans1", "Oceans2", "LMantle2", "UMantle2", "OCrust2"]
+COLUMNS = ["Atmosphere0", "CCrust2", "Oceans0", "Oceans1", "Oceans2", "LMantle2", "UMantle2", "OCrust2"]
 
 UNITS = {"Atmosphere0": "PAL",
+         "CCrust2": "PAL",
          "Oceans0": "mM",
          "Oceans1": "mM",
          "Oceans2": "mM",
@@ -22,6 +23,7 @@ UNITS = {"Atmosphere0": "PAL",
          "OCrust2": "ppm"}
 
 NORMS = {"Atmosphere0": 4e18,  # PAL
+         "CCrust2": 4e18,      # PAL
          "Oceans0": 3.8e16,    # mM ; molar = m*1e3 g/(28g/mol)/1.35e21 L
          "Oceans1": 5.1e16,    # mM ; average of 30 and 46 g/mol
          "Oceans2": 2.4e16,    # mM ; average of 17 and 18 g/mol
@@ -40,72 +42,56 @@ def get_full_data(field_, norm, time):
     '''Get data (time and field vectors) for a given time. Fetches output from
        all folders in DATA_DIRECTORY.'''
 
-    xval = []
-    yval = []
 
-    for folder in sorted(os.listdir(DATA_DIRECTORY)):
-        if not os.path.isdir(DATA_DIRECTORY+folder):
-            continue
+    folder = DATA_DIRECTORY
+    ndata = len(glob(folder+'/*.txt'))
 
-        folder = DATA_DIRECTORY + folder
-        ndata = len(glob(folder+'/*.txt'))
+    output = []
+    for data_file in sorted(glob(folder+'/*.txt')):
+        dataframe = pd.read_csv(data_file)
 
-        for data_file in glob(folder+'/*.txt'):
-            dataframe = pd.read_csv(data_file)
-            oxi = int(data_file.split("/")[-2][4:7])
+        split = data_file.split("_")
+        xlab = split[1]
+        ylab = split[3]
+        xx = float(split[2])
+        yy = float(split[4])
 
-            reservoir = dataframe[field_].values[time]
-            simtime = dataframe["time"].values[time]
+        reservoir = dataframe[field_].values[time]/norm
+        simtime = dataframe["time"].values[time]
 
-            xval.append(reservoir/norm)
-            yval.append(oxi)
+        output.append({'x':xx, 'y':yy, 'z':reservoir})
 
-    return xval, yval, simtime, ndata
-
-
+    a = sorted(output, key=lambda v:(v['x'],v['y']))
+    xval = [x['x'] for x in a]
+    yval = [x['y'] for x in a]
+    zval = [x['z'] for x in a]
+    return xval, yval, zval, simtime, ndata, xlab, ylab
 
 def make_time_plot(field_, norm, time):
     '''Makes the heatmap for a given time and field.'''
 
-    xval, yval, simtime, ndata = get_full_data(field_, norm, time)
+    xval, yval, zval, stime, ndata, xlab, ylab = get_full_data(field_, norm, time)
 
-    print(np.min(xval))
-    print(np.max(xval))
-
-    print(np.min(yval))
-    print(np.max(yval))
+    x, y = np.meshgrid(np.unique(xval), np.unique(yval))
+    z = np.reshape(zval, x.shape).T
 
     plt.figure()
 
-    xbins = np.logspace(-2, 2, 26)
-    ybins = np.linspace(np.min(yval), np.max(yval), 26)
-    counts, _, _ = np.histogram2d(xval, yval, bins=(xbins, ybins))
-    print(counts.shape)
-
-    plt.pcolormesh(xbins, ybins, counts.T)
+    plt.pcolormesh(x, y, z)
     plt.colorbar()
-    plt.clim(0, ndata)
+    plt.yscale('log')
 
-    plt.xscale('log')
-
-    plt.title("%s - %04d Ma" % (field_, simtime))
-    plt.xlabel("N-content [%s]" % UNITS[field_])
-    plt.ylabel("Redox state [-]")
+    plt.title("%s - %04d Ma [%s]" % (field_, stime, UNITS[field_]))
+    plt.xlabel(xlab)
+    plt.ylabel(ylab)
     plt.savefig("%s-%03d.png" % (field_, time), format="png")
     plt.close()
-
-
 
 def run_timeloop(field_):
     '''Makes all heatmaps for a given field'''
     print(field_)
     norm = NORMS[field_]
-    for i in range(180):
-        print(i)
-        make_time_plot(field_, norm, i)
+    make_time_plot(field_, norm, -1)
 
-
-
-#run_timeloop("Oceans1")
 for field in COLUMNS:
     run_timeloop(field)
